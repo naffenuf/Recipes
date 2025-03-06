4746ca458bd96d30724051b6a1354958f0355c8e
Craig Boyce <naffenuf@yahoo.com>
2025-03-06 14:40:11 -0500

    Initial Commit

---
+++ RecipesTests/RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by Craig Boyce on 3/6/25.
+//
+
+import Testing
+@testable import Recipes
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
